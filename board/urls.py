46a6aa81dd4e866ec55f1ec2ff787c74572be1b5
hjbc0921 <hjbc0921@gmail.com>
2018-12-16 21:15:16 +0900

    post delete view

--- board/urls.py
+++ board/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^create/$', views.post_create, name='post_create'),
     url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 ]
--- board/views.py
+++ board/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.template import loader
   
 def post_list(request):
+    if not request.user.is_authenticated:
+        return redirect('/signin')
     posts = Post.objects.order_by('-created_at')
     return render(request, 'board/post_list.html', {'posts' : posts, 'count' : len(posts)})
 
@@ -22,7 +24,20 @@ def post_create(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'board/post_detail.html', {'post' : post})
+    author = False
+    if (request.user == post.author):
+        author = True
+    next = int(pk)+1
+    try:
+        Post.objects.get(pk=next)
+    except:
+        next = 0
+    prev = int(pk)-1
+    try:
+        Post.objects.get(pk=str(prev))
+    except:
+        prev = 0
+    return render(request, 'board/post_detail.html', {'post' : post, 'next':next, 'prev':prev, 'author':author})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -30,9 +45,17 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'board/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('/posts')
+    else:
+        return render(request, 'board/post_delete.html', {'title': post.title})
+    
