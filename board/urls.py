89cdc641a0fca96e55b8275de10d0fa26dc6cec5
hjbc0921 <hjbc0921@gmail.com>
2018-12-16 18:49:44 +0900

    board view and url organized

--- board/urls.py
+++ board/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^create/$', views.post_create, name='post_create'),
+    url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
