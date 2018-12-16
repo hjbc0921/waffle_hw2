374a585f77614f433a6803c84979762a38596294
hjbc0921 <hjbc0921@gmail.com>
2018-12-16 23:08:04 +0900

    board css

--- board/views.py
+++ board/views.py
@@ -8,19 +8,16 @@ def post_list(request):
     if not request.user.is_authenticated:
         return redirect('/signin')
     posts = Post.objects.order_by('-created_at')
-    return render(request, 'board/post_list.html', {'posts' : posts, 'count' : len(posts)})
+    mine = Post.objects.filter(author=request.user)
+    return render(request, 'board/post_list.html', {'posts' : posts, 'count' : len(posts), 
+                            'user' : request.user.username, 'mine' : mine, 'mcount' : len(mine)})
 
 def post_create(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'board/post_edit.html', {'form': form})
+        post = Post(title=request.POST['title'], content=request.POST['content'], author=request.user)
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'board/post_edit.html', {'new':True, 'title':'', 'content':''})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -42,14 +39,11 @@ def post_detail(request, pk):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'board/post_edit.html', {'form': form})
+        post.title=request.POST['title']
+        post.content=request.POST['content']
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'board/post_edit.html', {'new': False, 'title': post.title, 'content': post.content})
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
